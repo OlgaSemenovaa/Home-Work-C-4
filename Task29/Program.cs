2a9e99245758b6b3360a8c6c1adf9085ed52ab50
Olga Semenova <olya.meoow@yandex.ru>
2022-11-12 19:17:03 +0300

    Task29 done

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -2,3 +2,30 @@
 // выводит их на экран.
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] CreateArray(int len)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(0, 1000); //поменять вторую цифру исходя из желаемого максимального значения
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < arr.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+
+Console.Write("Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+PrintArray(array);
